3b6bd08ff5d1fe19376e857eea5a9c9f887edaae
RAHUL YADAV <ryadav531996@gmail.com>
2018-09-09 11:48:02 +0530

    Blockchain basic code

--- blockchain.py
+++ blockchain.py
@@ -2,12 +2,19 @@ blockchain = [[1]]
 
 
 def get_last_value():
+    """Returns the last value of the current blockchain"""
     return blockchain[-1]
 
 def add_value(transaction_ammount,last_transaction_ammount=[1]):
-    blockchain.append([last_transaction_ammount,transaction_ammount])
+     """Append new value as well as the last block chain value to the blockchain
+     arguements:
+     :transaction_ammount:The ammount that should be added.
+     :last_transaction_ammount:The last transaction ammount(default[1])
+     """
+     blockchain.append([last_transaction_ammount,transaction_ammount])
 
 def get_user_input():
+    """Returns the input of the user as float"""
     return float(input("Enter the ammount: "))
 
    
